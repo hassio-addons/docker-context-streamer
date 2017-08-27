43c87597c4d3a17750459a3aa33e6264a555bd5e
Franck Nijhof <frenck@geekchimp.com>
2017-08-27 22:18:38 +0200

    Upped version to 0.0.3

--- dcs/__init__.py
+++ dcs/__init__.py
@@ -31,7 +31,7 @@ Docker using STDIN by creating a tar file stream.
 
 
 APP_NAME = 'docker-context-streamer'
-APP_VERSION = '0.0.2'
+APP_VERSION = '0.0.3'
 APP_DESCRIPTION = __doc__
 
 __author__ = 'Franck Nijhof'
@@ -39,5 +39,5 @@ __email__ = 'frenck@geekchimp.com'
 __copyright__ = 'Copyright 2017, Franck Nijhof'
 __license__ = 'GPLv3'
 __url__ = 'https://github.com/hassio-addons/docker-context-streamer'
-__download__ = 'https://github.com/hassio-addons/docker-context-streamer/archive/0.0.2.tar.gz'
+__download__ = 'https://github.com/hassio-addons/docker-context-streamer/archive/0.0.3.tar.gz'
 __version__ = APP_VERSION
