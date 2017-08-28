2fda17f49d0fce830dcf090f756fe12c8070d9e1
Franck Nijhof <frenck@geekchimp.com>
2017-08-28 19:46:36 +0200

    :art: Fixed PEP8 error

--- dcs/__init__.py
+++ dcs/__init__.py
@@ -39,5 +39,6 @@ __email__ = 'frenck@geekchimp.com'
 __copyright__ = 'Copyright 2017, Franck Nijhof'
 __license__ = 'GPLv3'
 __url__ = 'https://github.com/hassio-addons/docker-context-streamer'
-__download__ = 'https://github.com/hassio-addons/docker-context-streamer/archive/0.0.3.tar.gz'
+__download__ = ('https://github.com/hassio-addons/docker-context-streamer'
+                '/archive/0.0.3.tar.gz')
 __version__ = APP_VERSION
